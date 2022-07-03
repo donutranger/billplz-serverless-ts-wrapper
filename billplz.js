3ab1483d062541a243eba0b0728992ce86d42588
Haris Hadi <korehadonut@gmail.com>
2022-07-03 10:23:59 +0800

    feat(createBill): tested basics, committing for typescript migration

--- billplz.js
+++ billplz.js
@@ -2,38 +2,47 @@ const fetch = require("node-fetch");
 
 module.exports.getBill = async (event) => {
   console.log(event.pathParameters.id);
-  
+
+  const env = {
+    API_KEY: process.env.API_KEY,
+    API_ENDPOINT: process.env.API_ENDPOINT,
+    API_COLLECTION: process.env.API_COLLECTION,
+  };
+
+  console.log(env);
+
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Please input a bill ID",
+      message: "OK",
     }),
   };
 };
 
-module.exports.createBill = async () => {
-  const endpoint = "https://www.billplz-sandbox.com/api/v3/";
-  const key = "c7377ec0-50a6-4436-a8fb-19b3386cece6";
+module.exports.createBill = async (event) => {
+
+  console.log(JSON.parse(event.body));
   const args = {
-    collection_id: "lzofk7fz",
+    collection_id: process.env.API_COLLECTION,
     email: "harishadi.dev@gmail.com",
     mobile: "0134567890",
     name: "Haris",
     amount: 100,
-    callback_url: "https://mh-bp-fe.vercel.app/",
+    callback_url: "http://localhost:3000/",
     description: "test-transaction",
-    redirect_url: "https://mh-bp-fe.vercel.app/",
+    redirect_url: "http://localhost:3000/",
   };
 
-  const response = await fetch(`${endpoint}bills`, {
+  const response = await fetch(`${process.env.API_ENDPOINT}bills`, {
     method: "POST",
     mode: "cors",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
-      Authorization: "Basic " + new Buffer.from(key).toString("base64"),
+      Authorization:
+        "Basic " + new Buffer.from(process.env.API_KEY).toString("base64"),
     },
-    redirect: "follow",
+    redirect: "",
     body: JSON.stringify(args),
   });
 
