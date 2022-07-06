dbbe694375e8bc0bb8f21ac781262786d7862f28
Haris Hadi <korehadonut@gmail.com>
2022-07-06 22:32:13 +0800

    refactor: move keys to a reusable variable

--- handlers.ts
+++ handlers.ts
@@ -1,21 +1,23 @@
 import { BillArgs } from "./types";
 import fetch from "node-fetch";
 
+const API_KEY = Buffer.from(process.env.API_KEY ?? "", "utf8").toString(
+  "base64"
+);
+const API_ENDPOINT = process.env.API_ENDPOINT;
+const COLLECTION_ID = process.env.API_COLLECTION;
+
 module.exports.getBill = async (event) => {
   const id = event.pathParameters.id;
-  const encoded: string = Buffer.from(
-    process.env.API_KEY ?? "",
-    "utf8"
-  ).toString("base64");
 
-  const response = await fetch(`${process.env.API_ENDPOINT}bills/${id}`, {
+  const response = await fetch(`${API_ENDPOINT}bills/${id}`, {
     method: "GET",
     // KIV: Disable this once on prod
     mode: "cors",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
-      Authorization: "Basic " + encoded,
+      Authorization: "Basic " + API_KEY,
     },
   });
 
@@ -32,21 +34,17 @@ module.exports.getBill = async (event) => {
 module.exports.createBill = async (event) => {
   const args: BillArgs = {
     ...JSON.parse(event.body),
-    collection_id: process.env.API_COLLECTION,
+    collection_id: COLLECTION_ID,
   };
-  const encoded: string = Buffer.from(
-    process.env.API_KEY ?? "",
-    "utf8"
-  ).toString("base64");
 
-  const response = await fetch(`${process.env.API_ENDPOINT}bills`, {
+  const response = await fetch(`${API_ENDPOINT}bills`, {
     method: "POST",
     // KIV: Disable this once on prod
     mode: "cors",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
-      Authorization: "Basic " + encoded,
+      Authorization: "Basic " + API_KEY,
     },
     redirect: "",
     body: JSON.stringify(args),
@@ -57,5 +55,16 @@ module.exports.createBill = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(res),
+  };
+};
+
+module.exports.processBill = async (event) => {
+  console.log(event.body);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "OK",
+    }),
   };
 };
